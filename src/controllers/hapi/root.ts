554bc3ec0f8dd16a5824875ca876538f82b88c3a
Kyle <kyle@cloudversify.com>
2018-08-25 16:36:24 -0700

    added missing file

---
+++ src/controllers/hapi/root.ts
@@ -0,0 +1,22 @@
+import * as Hapi from "hapi";
+
+export default (server: Hapi.Server) => {
+
+  const basePath = "/";
+
+  server.route({
+    "method": "GET",
+    "path": basePath,
+    "options": {
+      "auth": false,
+      "tags": ["api"],
+      "description": "Get Server Status",
+      "notes": "Root API endpoint returns json object indicating server status"
+    },
+    "handler": (request) => {
+      return {
+        "status": "online"
+      };
+    }
+  });
+};
